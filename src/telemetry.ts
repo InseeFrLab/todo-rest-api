3046d9332edf8bc6a4e6b8ab367acfbd210dda08
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2026-05-19 11:14:59 +0200

    Bump dep and improved telemetry

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,21 +1,23 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 
 export function setupTracing() {
-    const sdk = new NodeSDK({
-        serviceName: "todo-rest-api",
-        traceExporter:
-            process.env.MODE === "dev"
-                ? new ConsoleSpanExporter()
-                : new OTLPTraceExporter()
+    
+    const traceExporter = new OTLPTraceExporter({
+        url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/traces`
     });
 
-    console.log(
-        process.env.MODE === "dev"
-            ? "Using ConsoleSpanExporter for tracing"
-            : "Using OTLPTraceExporter for tracing"
-    );
+    const spanProcessors = [
+        new SimpleSpanProcessor(traceExporter),
+        new SimpleSpanProcessor(new ConsoleSpanExporter())
+    ];
+
+    const sdk = new NodeSDK({
+        serviceName: "todo-rest-api",
+        spanProcessors
+    });
 
     sdk.start();
 }
