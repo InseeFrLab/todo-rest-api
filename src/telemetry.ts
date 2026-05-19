59cc055c90e23f3da36a61550a69079d04252806
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2026-05-19 11:38:11 +0200

    add diag

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,12 +1,14 @@
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 
 export function setupTracing() {
-    
+    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
+
     const traceExporter = new OTLPTraceExporter({
-        url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/traces`
+        url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`
     });
 
     const spanProcessors = [
