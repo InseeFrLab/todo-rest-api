5824cc83cee90a0ede987c27b4bcb192617f2fde
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2026-05-19 07:52:15 +0200

    fix

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,20 +1,21 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { ConsoleSpanExporter, SimpleSpanProcessor, BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 
 export function setupTracing() {
-    if (process.env.OTEL_ENABLED !== "true") {
-        return;
-    }
-
-    const isDevMode = process.env.MODE === "dev";
-
     const sdk = new NodeSDK({
         serviceName: "todo-rest-api",
-        spanProcessor: isDevMode
-            ? new SimpleSpanProcessor(new ConsoleSpanExporter())
-            : new BatchSpanProcessor(new OTLPTraceExporter())
+        traceExporter:
+            process.env.MODE === "dev"
+                ? new ConsoleSpanExporter()
+                : new OTLPTraceExporter()
     });
+
+    console.log(
+        process.env.MODE === "dev"
+            ? "Using ConsoleSpanExporter for tracing"
+            : "Using OTLPTraceExporter for tracing"
+    );
 
     sdk.start();
 }
